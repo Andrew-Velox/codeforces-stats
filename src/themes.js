4fc8f51043aa1476e421c22f9ce0b6513238d086
Andrew-Velox <mohabbat.bd2020@gmail.com>
2026-03-04 23:06:20 +0600

    add separator line

--- src/themes.js
+++ src/themes.js
@@ -11,7 +11,7 @@ const themes = {
     text_color: "000",
     bg_color: "fff",
     border_color: "e4e2e2",
-    box_border_color: "e4e2e2",
+    box_border_color: "94a3b8",
     tag_1_color: "06b6d4",
     tag_2_color: "ec4899",
     tag_3_color: "eab308",
@@ -174,96 +174,7 @@ const themes = {
     tag_3_color: "f0dfaf",
     chart_total_color: "fff",
   }
-  // tokyonight: {
-  //   title_color: "fff",
-  //   icon_color: "bf91f3",
-  //   text_color: "38bdae",
-  //   bg_color: "1a1b27",
-  // },
-  // monokai: {
-  //   title_color: "fff",
-  //   icon_color: "e28905",
-  //   text_color: "f1f1eb",
-  //   bg_color: "272822",
-  // },
-  // vue: {
-  //   title_color: "fff",
-  //   icon_color: "41b883",
-  //   text_color: "273849",
-  //   bg_color: "fffefe",
-  // },
-  // algolia: {
-  //   title_color: "fff",
-  //   icon_color: "2DDE98",
-  //   text_color: "FFFFFF",
-  //   bg_color: "050F2C",
-  // },
-  // gotham: {
-  //   title_color: "2aa889",
-  //   icon_color: "599cab",
-  //   text_color: "99d1ce",
-  //   bg_color: "0c1014",
-  // },
-  // "midnight-purple": {
-  //   title_color: "9745f5",
-  //   icon_color: "9f4bff",
-  //   text_color: "ffffff",
-  //   bg_color: "000000",
-  // },
-  // calm: {
-  //   title_color: "e07a5f",
-  //   icon_color: "edae49",
-  //   text_color: "ebcfb2",
-  //   bg_color: "373f51",
-  // },
-  // "flag-india": {
-  //   title_color: "ff8f1c",
-  //   icon_color: "250E62",
-  //   text_color: "509E2F",
-  //   bg_color: "ffffff",
-  // },
-  // react: {
-  //   title_color: "61dafb",
-  //   icon_color: "61dafb",
-  //   text_color: "ffffff",
-  //   bg_color: "20232a",
-  // },
-  // ocean_dark: {
-  //   title_color: "8957B2",
-  //   icon_color: "FFFFFF",
-  //   text_color: "92D534",
-  //   bg_color: "151A28",
-  // },
-  // city_lights: {
-  //   title_color: "5D8CB3",
-  //   icon_color: "4798FF",
-  //   text_color: "718CA1",
-  //   bg_color: "1D252C",
-  // },
-  // github_dark: {
-  //   title_color: "58A6FF",
-  //   icon_color: "1F6FEB",
-  //   text_color: "C3D1D9",
-  //   bg_color: "0D1117",
-  // },
-  // discord_old_blurple: {
-  //   title_color: "7289DA",
-  //   icon_color: "7289DA",
-  //   text_color: "FFFFFF",
-  //   bg_color: "2C2F33",
-  // },
-  // aura_dark: {
-  //   title_color: "ff7372",
-  //   icon_color: "6cffd0",
-  //   text_color: "dbdbdb",
-  //   bg_color: "252334",
-  // },
-  // panda: {
-  //   title_color: "19f9d899",
-  //   icon_color: "19f9d899",
-  //   text_color: "FF75B5",
-  //   bg_color: "31353a",
-  // },
+  
 };
 
 export default themes;
