5690798215c4679acb674360b81df09e7413295f
Andrew-Velox <mohabbat.bd2020@gmail.com>
2026-03-04 15:55:35 +0600

    update template and themes

--- src/themes.js
+++ src/themes.js
@@ -11,6 +11,7 @@ const themes = {
     text_color: "000",
     bg_color: "fff",
     border_color: "e4e2e2",
+    box_border_color: "e4e2e2",
     tag_1_color: "06b6d4",
     tag_2_color: "ec4899",
     tag_3_color: "eab308",
@@ -38,31 +39,140 @@ const themes = {
     icon_color: "fff",
     text_color: "fff",
     bg_color: "151515",
-    tag_1_color: "06b6d4",
-    tag_2_color: "ec4899",
-    tag_3_color: "eab308",
+    tag_1_color: "00d2ff",     // Electric Cyan (High Energy)
+    tag_2_color: "9d50bb",     // Deep Violet (Premium/Rare)
+    tag_3_color: "f00056",     // Crimson Rose (Action/Urgency)
     chart_total_color: "fff",
+    box_border_color: "e4e2e2",
   },
   nord_bright: {
     title_color: "000",
     icon_color: "000",
     text_color: "000",
     bg_color: "eceef5",
-    tag_1_color: "06b6d4",
-    tag_2_color: "ec4899",
-    tag_3_color: "eab308",
+    tag_1_color: "4361ee",     // Royal Electric Blue
+    tag_2_color: "7209b7",     // Deep Grape
+    tag_3_color: "f72585",     // Neon Raspberry
     chart_total_color: "000",
+    box_border_color: "94a3b8",
   },
   nord_dark: {
     title_color: "fff",
     icon_color: "fff",
     text_color: "fff",
-    bg_color: "151515",
-    tag_1_color: "06b6d4",
-    tag_2_color: "ec4899",
-    tag_3_color: "eab308",
+    bg_color: "2e3541",
+    tag_1_color: "0369a1", // Ocean Blue
+    tag_2_color: "0f766e", // Deep Teal
+    tag_3_color: "4338ca", // Indigo Night
     chart_total_color: "fff",
   },
+
+
+  radical: {
+    title_color: "fff",
+    // icon_color: "fe428e",
+    icon_color: "fff",
+    text_color: "fff",
+    bg_color: "141321",
+    border_color: "2a2845",
+    box_border_color: "2a2845",
+    tag_1_color: "fe428e",
+    tag_2_color: "a9fef7",
+    tag_3_color: "f8d847",
+    chart_total_color: "fff",
+  },
+
+  monokai: {
+    title_color: "fff",
+    // icon_color: "f92672",
+    icon_color: "fff",
+    text_color: "fff",
+    bg_color: "272822",
+    border_color: "3e3d32",
+    box_border_color: "3e3d32",
+    tag_1_color: "f92672",
+    tag_2_color: "a6e22e",
+    tag_3_color: "66d9ef",
+    chart_total_color: "fff",
+  },
+
+  gruvbox: {
+    title_color: "fff",
+    icon_color: "fe8019",
+    text_color: "fff",
+    bg_color: "282828",
+    border_color: "3c3836",
+    box_border_color: "3c3836",
+    tag_1_color: "cc241d",
+    tag_2_color: "98971a",
+    tag_3_color: "d79921",
+    chart_total_color: "fff",
+  },
+
+  solarized: {
+    title_color: "000",
+    icon_color: "268bd2",
+    text_color: "000",
+    bg_color: "fdf6e3",
+    border_color: "eee8d5",
+    box_border_color: "eee8d5",
+    tag_1_color: "268bd2",
+    tag_2_color: "d33682",
+    tag_3_color: "859900",
+    chart_total_color: "000",
+  },
+
+  solarized_dark: {
+    title_color: "fff",
+    icon_color: "fff",
+    text_color: "fff",
+    bg_color: "002b36",
+    border_color: "073642",
+    box_border_color: "073642",
+    tag_1_color: "268bd2",
+    tag_2_color: "d33682",
+    tag_3_color: "859900",
+    chart_total_color: "fff",
+  },
+
+  tokyonight: {
+    title_color: "fff",
+    icon_color: "7aa2f7",
+    text_color: "fff",
+    bg_color: "1a1b26",
+    border_color: "24283b",
+    box_border_color: "24283b",
+    tag_1_color: "9d7cd8",
+    tag_2_color: "7aa2f7",
+    tag_3_color: "f7768e",
+    chart_total_color: "fff",
+  },
+
+  vue: {
+    title_color: "000",
+    icon_color: "41b883",
+    text_color: "000",
+    bg_color: "ffffff",
+    border_color: "dfe2e5",
+    box_border_color: "dfe2e5",
+    tag_1_color: "41b883",
+    tag_2_color: "35495e",
+    tag_3_color: "81c784",
+    chart_total_color: "000",
+  },
+
+  zenburn: {
+    title_color: "000",
+    icon_color: "dfaf8f",
+    text_color: "000",
+    bg_color: "3f3f3f",
+    border_color: "4f4f4f",
+    box_border_color: "4f4f4f",
+    tag_1_color: "cc9393",
+    tag_2_color: "7f9f7f",
+    tag_3_color: "f0dfaf",
+    chart_total_color: "000",
+  }
   // tokyonight: {
   //   title_color: "fff",
   //   icon_color: "bf91f3",
